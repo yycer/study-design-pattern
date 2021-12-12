d3418eaec18290032729e29d6df14d4cd6876a40
姚屹晨 <yaoyichen@192.168.8.104>
2021-12-12 21:18:26 +0800

    First commit

---
+++ src/main/java/com/yyc/studydesignpattern/StudyDesignPatternApplication.java
@@ -0,0 +1,13 @@
+package com.yyc.studydesignpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyDesignPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyDesignPatternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yyc/studydesignpattern/StudyDesignPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yyc.studydesignpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyDesignPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
